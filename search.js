645366f0f78af45e100a6b86ceadb55fd9f7118d
minki1220 <130380152+minki1220@users.noreply.github.com>
2023-05-29 22:40:25 +0900

    23.05.29

--- search.js
+++ search.js
@@ -1,9 +1,17 @@
-const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+const searchInput = document.querySelector(".search-input");
 
-searchButton.addEventListener("click", performSearch);
-
-function performSearch() {
-  const searchTerm = searchInput.value;
-  console.log("검색어:", searchTerm);
+function performSearch(searchText) {
+  // 여기에 실제 검색 로직을 구현하세요.
+  alert(`검색어: ${searchText}`);
 }
+
+function onSearchClick(event) {
+  event.preventDefault();
+  const searchText = searchInput.value.trim();
+  if (searchText !== "") {
+    performSearch(searchText);
+  }
+}
+
+searchButton.addEventListener("click", onSearchClick);
